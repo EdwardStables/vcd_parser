6b738d6b19cc384fc3637aa156b73f9ace44fe87
Edward Stables <edward.stables1198@gmail.com>
2023-07-02 00:21:39 +0100

    actually overriding top level entry

--- headers/listener.h
+++ headers/listener.h
@@ -2,6 +2,6 @@
 #include "antlr4-runtime.h"
 
 class VCDListener : public antlrvcdp::VCDParserBaseListener {
-
+  virtual void enterValue_change_dump_definitions(antlrvcdp::VCDParser::Value_change_dump_definitionsContext*) override;
   virtual void enterDeclaration_command(antlrvcdp::VCDParser::Declaration_commandContext*) override;
 };
--- src/listener.cpp
+++ src/listener.cpp
@@ -1,8 +1,8 @@
 #include "listener.h"
 #include <iostream>
 
-void enterValue_change_dump_definitions(antlrvcdp::VCDParser::Value_change_dump_definitionsContext* ctx){
-    std::cout << "top" << std::endl;
+void VCDListener::enterValue_change_dump_definitions(antlrvcdp::VCDParser::Value_change_dump_definitionsContext* ctx){
+    std::cout << ctx->getText() << std::endl;
 }
 
 void VCDListener::enterDeclaration_command(antlrvcdp::VCDParser::Declaration_commandContext* ctx){
